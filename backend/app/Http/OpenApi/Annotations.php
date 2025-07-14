c5857ceb80c6027b2628eef361e00cead4a16889
wasee-sun <revengersun@gmail.com>
2025-07-14 21:50:09 +0600

    User Controller Request and Route created.
    Co-authored-by: Ariana Suha <arianasuha@users.noreply.github.com>

---
+++ backend/app/Http/OpenApi/Annotations.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\OpenApi;
+
+/**
+ * @OA\Info(
+ *     title="My Laravel API",
+ *     version="1.0.0",
+ *     description="API documentation for my Laravel project",
+ *     @OA\Contact(
+ *         email="support@example.com"
+ *     )
+ * )
+ */
+class Annotations
+{
+}
