c81e745c02f500eb016c41315e03c626d03ef818
wasee-sun <revengersun@gmail.com>
2025-07-18 10:54:57 +0600

    Removed laravel sanctum will start fresh

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,6 +7,3 @@ use App\Http\Controllers\Auth\LoginController;
 Route::post('/register', [RegisterController::class, 'register'])
 ->name('api.register');
 
-Route::post('/login', [LoginController::class, 'login'])->name('api.login');
-
-Route::middleware('auth:sanctum')->post('/logout', [LoginController::class, 'logout'])->name('api.logout');
